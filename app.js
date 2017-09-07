7aecd2230d96f165720c3017f2be6569d74ecab6
Niral Pokal <niralpokal@gmail.com>
2017-09-06 22:32:03 -0700

    Moved app.js to root folder

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 
-app.use(express.static('../public/'));
+app.use(express.static('./public/'));
 
 app.get('/', (req, res)=>{
     res.send();
