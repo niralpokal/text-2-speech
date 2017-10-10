23e42105689cbf08adffe62fd96fc40c2736a8c3
Niral Pokal <niral@Nirals-MacBook-Pro.local>
2017-10-09 21:41:00 -0700

    Added typings for JQuery and corrected some typescript errors.

--- public/app.js
+++ public/app.js
@@ -1,41 +1,39 @@
-$(document).ready(function () { return $('select').material_select(); });
-var getObj = function (id) { return document.getElementById(id); };
-var speed = getObj('speedSlide');
-var pitch = getObj('pitchSlide');
-var voiceOptions = getObj('voiceOptions');
-var speechText = getObj('speakText');
-var speechButton = getObj('speakButton');
-var synth = window.speechSynthesis;
-var isChrome = (window.chrome && window.chrome.webstore);
-var populateList = function () {
-    var appendList = function () {
-        var voices = synth.getVoices();
-        for (var _i = 0, voices_1 = voices; _i < voices_1.length; _i++) {
-            var voice = voices_1[_i];
-            var option = document.createElement('option');
-            option.textContent = voice.name + " " + voice.lang + " " + (voice["default"] ? '-Default' : '');
+$(document).ready(() => $('select').material_select());
+const getObj = (id) => document.getElementById(id);
+const speed = getObj('speedSlide');
+const pitch = getObj('pitchSlide');
+const voiceOptions = getObj('voiceOptions');
+const speechText = getObj('speakText');
+const speechButton = getObj('speakButton');
+const synth = window.speechSynthesis;
+const isChrome = (window.chrome && window.chrome.webstore);
+let populateList = () => {
+    let appendList = () => {
+        let voices = synth.getVoices();
+        for (let voice of voices) {
+            let option = document.createElement('option');
+            option.textContent = `${voice.name} ${voice.lang} ${voice.default ? '-Default' : ''}`;
             option.setAttribute('data-name', voice.name);
             voiceOptions.appendChild(option);
         }
     };
     if (isChrome) {
-        var getVoices = new Promise(function (done) { return synth.onvoiceschanged = done; });
-        getVoices.then(function () { return appendList(); });
+        const getVoices = new Promise(done => synth.onvoiceschanged = done);
+        getVoices.then(() => appendList());
     }
     else {
         appendList();
     }
 };
 populateList();
-var handleSpeak = function () {
+let handleSpeak = () => {
     synth.cancel();
-    var voices = synth.getVoices();
-    var selectedOption = voiceOptions.selectedOptions[0].getAttribute('data-name');
-    var utter = new SpeechSynthesisUtterance(speechText.value);
+    let voices = synth.getVoices();
+    const selectedOption = voiceOptions.selectedOptions[0].getAttribute('data-name');
+    let utter = new SpeechSynthesisUtterance(speechText.value);
     utter.rate = parseFloat(speed.value);
     utter.pitch = parseFloat(pitch.value);
-    for (var _i = 0, voices_2 = voices; _i < voices_2.length; _i++) {
-        var voice = voices_2[_i];
+    for (let voice of voices) {
         if (voice.name === selectedOption) {
             utter.voice = voice;
             break;
@@ -43,14 +41,14 @@ var handleSpeak = function () {
     }
     synth.speak(utter);
 };
-document.addEventListener('click', function (event) {
+document.addEventListener('click', (event) => {
     if (event.target == speechButton) {
         event.preventDefault();
         handleSpeak();
     }
 });
-document.addEventListener('keydown', function (event) {
-    var key = event.which || event.keyCode;
+document.addEventListener('keydown', (event) => {
+    const key = event.which || event.keyCode;
     if (key === 13) {
         event.preventDefault();
         handleSpeak();
